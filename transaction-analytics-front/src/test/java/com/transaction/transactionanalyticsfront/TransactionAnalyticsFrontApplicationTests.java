8f1c08cb9a70591f2e2b54d36927558d2a8ec2be
lanyablue <barbaramy_@hotmail.com>
2022-07-29 20:40:17 -0300

    spring boot

---
+++ transaction-analytics-front/src/test/java/com/transaction/transactionanalyticsfront/TransactionAnalyticsFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transaction.transactionanalyticsfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionAnalyticsFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
